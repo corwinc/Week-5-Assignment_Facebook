6d908c0faf1af1c59ed48ed6c34a9ac243be2e24
Corwin Crownover <corwincrownover@corwins-macbook-air.local>
2016-03-03 07:39:18 -0800

    Initial Commit

---
+++ Week 5 Assignment_Facebook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Week 5 Assignment_Facebook
+//
+//  Created by Corwin Crownover on 3/3/16.
+//  Copyright © 2016 Corwin Crownover. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
